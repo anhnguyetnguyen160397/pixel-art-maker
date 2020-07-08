4fdb47e8653079216c75fbbba406605746e72903
Nguyen Thi Anh Nguyet <anhnguyetnguyen16397@gmail.com>
2020-07-08 10:08:31 +0700

    second commit with VueJs

--- designs.js
+++ designs.js
@@ -1,57 +1,68 @@
-let form = document.getElementById('sizePicker');
-let table = document.getElementById('pixelCanvas');
-let heightInput = document.getElementById('inputHeight');
-let widthInput = document.getElementById('inputWidth');
+Vue.component('display',{
+    template:
+    `
+    <div>
+        <div id='choose-size'>
+            <h2>Choose Grid Size</h2>
+            <form id="sizePicker">
+                <label for='inputHeight'><label for='name'>Grid Height:</label></label>
+                <input type="number" id="inputHeight" name="height" min="1" value="1" v-model='height'>
+                <label for='inputWidth'><label for='name'>Grid Width:</label></label>
+                <input type="number" id="inputWidth" name="width" min="1" value="1" v-model='width'>
+                <input type="submit" id='button' @click.prevent='makeGrid' @click='fillColor'>
+            </form>
+        </div>
 
 
-form.onsubmit = function (event){
-    event.preventDefault();
-    height = heightInput.value;
-    width = widthInput.value;
-    makeGrid(height,width);
-    fillColor();
-}
+        <div id='pick-color'>
+            <h2>Pick A Color</h2>
+            <input type="color" id="colorPicker" v-model='color'>
+        </div>
 
+       
+        <div id='design-canvas'>
+            <h2>Design Canvas</h2>
+            <table id="pixelCanvas">
+                <tbody>
+                <tr v-for="index in parseInt(height)">
+                    <td v-for="column in parseInt(width)"  @click.prevent='fillColor($event)'>
+                    </td>
+                    
+                </tr>
+                </tbody>
 
-function makeGrid(){
-    for (var i = 0; i<height; i++){
-        var tr = table.insertRow(i);
-        for (var j = 0; j<width; j++){
-            var td = tr.insertCell(j);
+            </table>
+        </div>
+    </div>
+    `,
+    data(){
+        
+        return{
+            height: 0,
+            width: 0,
+            color:'#f6b73c',
+        }
+    },
+    methods:{
+        fillColor(event) {
+            targetId = event.currentTarget
+            console.log(targetId)
+            console.log(this.color)
+            targetId.style.backgroundColor=this.color
         }
     }
-}
+})
 
-function fillColor(){
-    cells = document.getElementsByTagName('td');
-    for (var i = 0; i<cells.length; i++){
-        var item =cells[i];
-        item.addEventListener('click', (function(_item) {
-            var color = document.getElementById('colorPicker').value;
-            _item.style.backgroundColor = color;
-        }).bind(this, item))
+
+
+var app=new Vue(
+    {
+        el: '#main',
+        data:{
+    
+        },
+        methods:{
+            
+        }
     }
-}
-
-/*
-function fillColor(){
-    cells = document.getElementsByTagName('td');
-    var arr = Array.prototype.slice.call( cells )
-    arr.forEach (cellFunction);
-} 
-*/   
-
-
-// function cellFunction (item){
-//     item.addEventListener('click',function(){
-//         var color = document.getElementById('colorPicker').value;
-//         item.style.backgroundColor = color;
-//     })
-// }
-
-// for (var i = 0; i<cells.length; i++){
-//     cells[i].addEventListener('click', function(){
-//         var color = document.getElementById('colorPicker').value;
-//         cells[i].style.backgroundColor = color;
-//     })
-// }
+);
